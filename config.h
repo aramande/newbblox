9a28e4e520c9b669b5e42f4f0851d985a4479d96
Aramande <aramande@hackular.com>
2009-10-15 00:45:41 +0200

    changed version number to version 1.0.1

--- config.h
+++ config.h
@@ -50,16 +50,16 @@
 #define PACKAGE_NAME "newbblox"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "newbblox 0.9"
+#define PACKAGE_STRING "newbblox 1.0.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "newbblox"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.9"
+#define PACKAGE_VERSION "1.0.1"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.9"
+#define VERSION "1.0.1"
